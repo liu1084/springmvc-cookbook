2e0e6ffeafdb37a7ec6a63076efd857320144ef4
jim <liu1084@163.com>
2017-01-11 00:36:35 +0800

    添加了servlet-api依赖包

--- cloudstreetmarket-parent/cloudstreetmarket-webapp/src/main/java/com/jim/controller/PageController.java
+++ cloudstreetmarket-parent/cloudstreetmarket-webapp/src/main/java/com/jim/controller/PageController.java
@@ -1,12 +1,10 @@
 package com.jim.controller;
 
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
